3e349e20bb1996ca5ba4f3a9dc28e287abd63acb
SolitaryXero <103876121+SolitaryXero@users.noreply.github.com>
2023-10-31 22:47:43 +0500

    fix the sketchPad, created a variable that kept track of an event

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 // create grid
 
-let colorValue = "#ff0000";
-let minSquares = 1;
-let maxSquares = 64;
+let selectColor = "black";
+const squaresMin = 1;
+const squaresMax = 64;
 let noOfSquares = 16;
 let totalSquares = noOfSquares*noOfSquares;
 
+let eventMouseDown = false;
+
 let sketchPad = document.querySelector(".sketchPad");
 let btnChangeGridSize = document.querySelector(".btnChangeNoOfSquares");
-let btnClearGrid = document.querySelector(".btnClear");
+let btnClear = document.querySelector(".btnClear");
 let gridColor = document.querySelector(".gridColor");
+
+console.log(gridColor.value)
+gridColor.addEventListener("input", ()=>{
+    selectColor = gridColor.value;
+    gridColor.style.backgroundColor = gridColor.value;
+})
+
+sketchPad.onmousedown = () => (eventMouseDown = true)
+sketchPad.onmouseup = () => (eventMouseDown = false)
+
+createGrid(noOfSquares);
+
+// Grid Creation
+function createGrid(noOfSquares){
+    for(let i = 0; i < noOfSquares * noOfSquares; i++){
+        let grid = document.createElement("div");
+        grid.setAttribute("class", "newSquare");
+        grid.setAttribute("style", `width: ${500/noOfSquares}px; height: ${500/noOfSquares}px;`);
+        gridEvents(grid);
+        
+        sketchPad.appendChild(grid);       
+    }
+}
+
+// Grid Events
+function gridEvents(grid){
+    grid.addEventListener("mouseenter", ()=>{
+        if(eventMouseDown){
+            grid.style.backgroundColor = `${selectColor}`;
+        }  
+    })
+}
 
 btnChangeGridSize.addEventListener("click", ()=>{
     squares = prompt("Please provide the grid size, between 1 and 100");
     if (squares === NaN){
         alert("Invalid input");
-    } else if (squares >= minSquares && squares <= maxSquares){
+    } else if (squares >= squaresMin && squares <= squaresMax){
         deleteGrid(noOfSquares);
         noOfSquares = squares;
         createGrid(noOfSquares);
@@ -23,41 +57,15 @@ btnChangeGridSize.addEventListener("click", ()=>{
     console.log(squares)
 })
 
-btnClearGrid.addEventListener("click", ()=>{
+btnClear.addEventListener("click", ()=>{
     deleteGrid(noOfSquares);
     createGrid(noOfSquares);
 })
 
-createGrid(noOfSquares);
-changeGridColor();
-
-function changeGridColor(){
-    
-    gridColor.addEventListener("input",()=>{
-        colorValue = gridColor.value;
-    })
-}
-console.log(gridColor.value);
 function deleteGrid(noOfSquares){
     for(let i = 0; i < noOfSquares * noOfSquares; i++){
-        
         let div = document.querySelector(".newSquare");
         div.remove();
     }
-
 }
 
-function createGrid(noOfSquares){
-
-    for(let i = 0; i < noOfSquares * noOfSquares; i++){
-        let div = document.createElement("div");
-        div.setAttribute("class", "newSquare");
-        div.setAttribute("style", `width: ${500/noOfSquares}px; height: ${500/noOfSquares}px;`);
-        
-        console.log(colorValue)
-        div.addEventListener("mouseenter", ()=>{
-            div.style.backgroundColor =`${colorValue}`;
-        })
-        sketchPad.appendChild(div);
-    }
-}
