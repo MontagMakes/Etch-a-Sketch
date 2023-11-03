33075cfe76c9a912e9d92fb3c41f3607b1395f67
SolitaryXero <103876121+SolitaryXero@users.noreply.github.com>
2023-11-03 20:13:01 +0500

    Added button behavior

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 // create grid
 
-let selectColor = "black";
-const squaresMin = 1;
+const sketchPad = document.querySelector(".sketchPad");
+const inputColor = document.querySelector(".inputColor");
+const btnColorMode = document.querySelector(".btnColorMode");
+const inputGridSize = document.querySelector(".inputGridSize");
+const btnClear = document.querySelector(".btnClear");
+const btnRandomMode = document.querySelector(".btnRandomMode")
+const btnEraser = document.querySelector(".btnEraser");
+
+const gridSize = document.querySelector(".gridSize")
+
+let selectedMode = "eraser";
+let selectColor = "lightblue";
+
+const squaresMin = 2;
 const squaresMax = 64;
-let noOfSquares = 16;
-let totalSquares = noOfSquares*noOfSquares;
+let noOfSquares = inputGridSize.getAttribute("value");
 
 let eventMouseDown = false;
 
-let sketchPad = document.querySelector(".sketchPad");
-let btnChangeGridSize = document.querySelector(".btnChangeNoOfSquares");
-let btnClear = document.querySelector(".btnClear");
-let btnColor = document.querySelector(".btnColor");
 
+main()
 
-btnColor.addEventListener("input", ()=>{
-    selectColor = btnColor.value;
-    btnColor.style.backgroundColor = btnColor.value;
-})
-
-sketchPad.onmousedown = () => (eventMouseDown = true)
-sketchPad.onmouseup = () => (eventMouseDown = false)
-
-createGrid(noOfSquares);
-
-inputColor()
-function inputColor(){
-    const span = document.querySelector("span");
-    const input = document.querySelector(".btnColor");    
-    span.addEventListener("click", ()=>{
-        span.style.backgroundColor = input.value
-        input.click();
-        
-    })
+function main(){
+    createGrid(noOfSquares);
     
+    modeSelect();
+    buttonColorMode();
+    buttonRandomMode();
+    buttonEraser();
+    buttonClear();
+    gridSizeChanger();
 }
 
 // Grid Creation
@@ -44,35 +41,94 @@ function createGrid(noOfSquares){
         grid.setAttribute("style", `width: ${500/noOfSquares}px; height: ${500/noOfSquares}px;`);
         gridEvents(grid);
         
-        sketchPad.appendChild(grid);       
+        sketchPad.appendChild(grid);
+        colorInput();
     }
 }
 
-// Grid Events
+// Selected Mode
+function modeSelect(){
+    if(selectedMode == "color"){
+        selectColor = inputColor.value;
+        btnRandomMode.style.backgroundColor = "#ededed"
+        btnColorMode.style.backgroundColor = "lightblue";
+        btnEraser.style.backgroundColor = "#ededed";
+    } else if(selectedMode == "Random"){
+        selectColor = getRandomColor();
+        btnRandomMode.style.backgroundColor = "lightblue"
+        btnColorMode.style.backgroundColor = "#ededed";
+        btnEraser.style.backgroundColor = "#ededed";
+    } else if (selectedMode == "eraser"){
+        selectColor = "white";
+        btnRandomMode.style.backgroundColor = "#ededed"
+        btnColorMode.style.backgroundColor = "#ededed";
+        btnEraser.style.backgroundColor = "lightblue";
+    }
+}
+
+// Color mode behaviour
+function buttonColorMode(){
+    
+    btnColorMode.addEventListener("click", ()=>{    
+        selectedMode = "color";
+        console.log(selectedMode);
+        modeSelect()
+    })
+}
+
+// Random mode behaviour
+function buttonRandomMode() {
+    
+    btnRandomMode.addEventListener("click", ()=>{
+        selectedMode = "Random";
+        console.log(selectedMode);
+        modeSelect();
+    })
+}
+
+// button eraser behavior
+function buttonEraser() {
+
+    btnEraser.addEventListener("click", ()=>{
+        selectedMode = "eraser";
+        console.log(selectedMode);
+        modeSelect()
+    });
+}
+
+// Clear button behavior
+function buttonClear(){
+    btnClear.addEventListener("click", ()=>{
+        deleteGrid(noOfSquares);
+        createGrid(noOfSquares);
+    })
+}
+
+// input type range 
+function gridSizeChanger() {
+
+    inputGridSize.addEventListener("input", ()=>{
+        gridSize.textContent = inputGridSize.value + " x " + inputGridSize.value;
+    })
+    
+    inputGridSize.addEventListener("mouseup", (event)=>{
+        deleteGrid(noOfSquares);
+        noOfSquares = event.target.value
+        createGrid(noOfSquares);
+    })
+}
+
 function gridEvents(grid){
+    sketchPad.onmousedown = () => (eventMouseDown = true)
+    sketchPad.onmouseup = () => (eventMouseDown = false)
+
     grid.addEventListener("mouseenter", ()=>{
         if(eventMouseDown){
             grid.style.backgroundColor = `${selectColor}`;
         }  
     })
+    
 }
-
-btnChangeGridSize.addEventListener("click", ()=>{
-    squares = prompt("Please provide the grid size, between 1 and 100");
-    if (squares === NaN){
-        alert("Invalid input");
-    } else if (squares >= squaresMin && squares <= squaresMax){
-        deleteGrid(noOfSquares);
-        noOfSquares = squares;
-        createGrid(noOfSquares);
-    }
-    console.log(squares)
-})
-
-btnClear.addEventListener("click", ()=>{
-    deleteGrid(noOfSquares);
-    createGrid(noOfSquares);
-})
 
 function deleteGrid(noOfSquares){
     for(let i = 0; i < noOfSquares * noOfSquares; i++){
@@ -80,4 +136,18 @@ function deleteGrid(noOfSquares){
         div.remove();
     }
 }
-
+function colorInput(){
+    inputColor.addEventListener("input",()=>{
+        modeSelect();
+    
+        console.log(inputColor.value)
+    })    
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
